46c8e1f77ce11e86ecbb7bc535edbd68d1325a6c
huydangdh <huydang.dh@mt2015.com>
2016-08-10 19:26:17 +0700

    Update api-dream-scope.js

--- modules/api-dream-scope.js
+++ modules/api-dream-scope.js
@@ -1,8 +1,9 @@
 /**
  * Created by QH2107 on 30/07/2016.
  */
-
-var request = require('request'), cachedRequest = require('cached-request')(request), cacheDirectory = "/tmp/cache";
+var request = require('request'),
+    cachedRequest = require('cached-request')(request),
+    cacheDirectory = "/tmp/cache";
 cachedRequest.setCacheDirectory(cacheDirectory);
 
 var zlib = require('zlib');
@@ -10,52 +11,42 @@ var har = {
     "method": "",
     "url": "",
     "httpVersion": "HTTP/1.1",
-    "headers": [
-        {
-            "name": "Origin",
-            "value": "https://dreamscopeapp.com"
-        },
-        {
-            "name": "Host",
-            "value": "dreamscopeapp.com"
-        },
-        {
-            "name": "Accept-Language",
-            "value": "vi,en;q=0.8"
-        },
-        {
-            "name": "Accept-Encoding",
-            "value": "gzip, deflate, sdch"
-        },
-        {
-            "name": "User-Agent",
-            "value": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"
-        },
-        {
-            "name": "Content-Type",
-            "value": "application/json;charset=UTF-8"
-        },
-        {
-            "name": "Accept",
-            "value": "application/json, text/plain, */*"
-        },
-        {
-            "name": "Referer",
-            "value": "https://dreamscopeapp.com/editor"
-        },
-        {
-            "name": "x-apicache-bypass",
-            "value": "false"
-        },
-        {
-            "name": "Connection",
-            "value": "keep-alive"
-        }],
-        "postData": {
-            "mimeType": "application/json;charset=UTF-8",
-            "text": "{\"private\":false,\"retain_color\":\"0\",\"processing_status\":2,\"image_base64\":\"\",\"filter\":\"blue_anime\"}"
-        }
-    };
+    "headers": [{
+        "name": "Origin",
+        "value": "https://dreamscopeapp.com"
+    }, {
+        "name": "Host",
+        "value": "dreamscopeapp.com"
+    }, {
+        "name": "Accept-Language",
+        "value": "vi,en;q=0.8"
+    }, {
+        "name": "Accept-Encoding",
+        "value": "gzip, deflate, sdch"
+    }, {
+        "name": "User-Agent",
+        "value": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"
+    }, {
+        "name": "Content-Type",
+        "value": "application/json;charset=UTF-8"
+    }, {
+        "name": "Accept",
+        "value": "application/json, text/plain, */*"
+    }, {
+        "name": "Referer",
+        "value": "https://dreamscopeapp.com/editor"
+    }, {
+        "name": "x-apicache-bypass",
+        "value": "false"
+    }, {
+        "name": "Connection",
+        "value": "keep-alive"
+    }],
+    "postData": {
+        "mimeType": "application/json;charset=UTF-8",
+        "text": "{\"private\":false,\"retain_color\":\"0\",\"processing_status\":2,\"image_base64\":\"\",\"filter\":\"blue_anime\"}"
+    }
+};
 
 /**
  * Gửi request đến API DreamScope.
@@ -67,7 +58,7 @@ var har = {
 
 function sendRequest(url, imageBase64, nameFilter, callback) {
     har.headers.url = url;
-    har.postData.text = "{\"private\":false,\"retain_color\":\"0\",\"processing_status\":2,\"image_base64\":\""+ imageBase64 +"\",\"filter\":\"" + nameFilter + "\"}";
+    har.postData.text = "{\"private\":false,\"retain_color\":\"0\",\"processing_status\":2,\"image_base64\":\"" + imageBase64 + "\",\"filter\":\"" + nameFilter + "\"}";
 
     cachedRequest({
         method: "POST",
@@ -75,8 +66,10 @@ function sendRequest(url, imageBase64, nameFilter, callback) {
         har: har,
         encoding: null
     }, function postResponse(err, res, body) {
-        if(err) {throw new Error(err);}
-        zlib.gunzip(body, function (err, unzipped) {
+        if (err) {
+            throw new Error(err);
+        }
+        zlib.gunzip(body, function(err, unzipped) {
             var data = unzipped.toString('utf-8');
             return callback(data);
         });
@@ -103,8 +96,10 @@ function getImages(limit, offset, callback) {
         har: har,
         encoding: null
     }, function getResponse(err, res, body) {
-        if(err) {throw new Error(err);}
-        zlib.gunzip(body, function (err, unzipped) {
+        if (err) {
+            throw new Error(err);
+        }
+        zlib.gunzip(body, function(err, unzipped) {
             var data = unzipped.toString('utf-8');
             return callback(data);
         });
@@ -123,20 +118,24 @@ function imageStatus(uuid, callback) {
     har.headers.method = "GET";
     har.postData.mimeType = null;
     har.postData.text = null;
-    
+
     request({
             method: "GET",
             uri: url,
             har: har,
             encoding: null
-    },
+        },
         function imageStatusResponse(err, res, body) {
-            if(err) {throw new Error(err);}
-            zlib.gunzip(body, function (err, unzipped) {
+            if (err) {
+                throw new Error(err);
+            }
+            zlib.gunzip(body, function(err, unzipped) {
                 var data = unzipped.toString('utf-8');
                 var json = JSON.parse(data);
 
-                if(json.processing_status === 2) {return callback(data);}
+                if (json.processing_status === 2) {
+                    return callback(data);
+                }
                 return callback("{\"processing_status\": 0,\"uuid\": \"" + uuid + "\"}");
             });
         });
